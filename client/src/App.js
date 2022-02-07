04173c7ea86f852a9f094131767d368d8640207d
Laura Travin <laura.travin@gmail.com>
2022-02-07 16:50:28 -0500

    Registration fornt end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+
+  const [usernameReg, setUsernameReg] = useState(' ')
+  const [passwordReg, setPasswordReg] = useState(' ')
+
   return (
     <div className="App">
       <header className="App-header">
         
-        
-          App in progress
+          <div className="App">
+            <div className="registration">
+                <h1>Registration</h1>
+                <label>Username</label>
+                <input type="text" onChange={(e)=>{setUsernameReg(e.target.value)}}/><br/>
+                <label>Password</label>
+                <input type="pasword" onChange={(e)=>{setPasswordReg(e.target.value)}}></input><br/>
+                <button onClick={()=>{}}>Register</button>
+            </div>
+          </div>
+     
       
       </header>
     </div>
