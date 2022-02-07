de99047a0efb4a1ea6933f42a9d77d992bd2c9b0
Laura Travin <laura.travin@gmail.com>
2022-02-07 18:33:46 -0500

    login

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,22 @@ function App() {
 
   const [usernameReg, setUsernameReg] = useState(' ')
   const [passwordReg, setPasswordReg] = useState(' ')
+  const [usernameLog, setUsernameLog] = useState(' ')
+  const [passwordLog, setPasswordLog] = useState(' ')
 
   const register= () => {
      Axios.post('http://localhost:3001/register', {username: usernameReg, password: passwordReg})
      .then((response) =>  console.log(response))
   }
 
+  const login= () => {
+    Axios.post('http://localhost:3001/login', {username: usernameLog, password: passwordLog})
+    .then((response) =>  console.log(response))
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
+    
+    
         
           <div className="App">
             <div className="registration">
@@ -26,11 +33,19 @@ function App() {
                 <input type="password" onChange={(e)=>{setPasswordReg(e.target.value)}}></input><br/>
                 <button onClick={register}>Register</button>
             </div>
+            <div className="registration">
+                <h1>Registration</h1>
+                <label>Username</label>
+                <input type="text" onChange={(e)=>{setUsernameLog(e.target.value)}}/><br/>
+                <label>Password</label>
+                <input type="password" onChange={(e)=>{setPasswordLog(e.target.value)}}></input><br/>
+                <button onClick={login}>Login</button>
+            </div>
           </div>
      
       
-      </header>
-    </div>
+     
+   
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ app.use(express.json())
 
 
 app.post('/register', (req, res)=> {
-
   const username= req.body.username;
   const password= req.body.password;
   console.log("username: ", username, "password: ", password  )
@@ -16,6 +15,27 @@ app.post('/register', (req, res)=> {
     console.log(err)
   })
 })
+
+app.post('/login', (req, res)=> {
+  const username= req.body.username;
+  const password= req.body.password;
+  
+  db.query("SELECT * FROM login WHERE username = ? AND password= ?", [username, password], (err, result) => {
+    
+    if (err){
+       res.send(err) //if error res sends error and stop
+      } 
+    if(result.length > 0){
+      res.send(result)
+    } else {
+      res.send({message: "Wrong username/password"})
+    }
+    
+  })
+})
+
+
+
 
 app.listen(3001,() => {
   console.log("reminder server running")
