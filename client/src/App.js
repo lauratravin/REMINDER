0fc4a4aba312ebfccef34caa76dada2cb0d16f1b
Laura Travin <laura.travin@gmail.com>
2022-02-07 18:08:25 -0500

    registartion db

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React, { useState } from "react";
+import Axios from 'axios';
 
 function App() {
 
 
   const [usernameReg, setUsernameReg] = useState(' ')
   const [passwordReg, setPasswordReg] = useState(' ')
+
+  const register= () => {
+     Axios.post('http://localhost:3001/register', {username: usernameReg, password: passwordReg})
+     .then((response) =>  console.log(response))
+  }
 
   return (
     <div className="App">
@@ -17,8 +23,8 @@ function App() {
                 <label>Username</label>
                 <input type="text" onChange={(e)=>{setUsernameReg(e.target.value)}}/><br/>
                 <label>Password</label>
-                <input type="pasword" onChange={(e)=>{setPasswordReg(e.target.value)}}></input><br/>
-                <button onClick={()=>{}}>Register</button>
+                <input type="password" onChange={(e)=>{setPasswordReg(e.target.value)}}></input><br/>
+                <button onClick={register}>Register</button>
             </div>
           </div>
      
--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const db = require('./db')
 app.use(cors())
 app.use(express.json())
 
+
+app.post('/register', (req, res)=> {
+
+  const username= req.body.username;
+  const password= req.body.password;
+  console.log("username: ", username, "password: ", password  )
+  db.query("INSERT INTO login (username,password) VALUES (?,?)", [username, password], (err, result) => {
+    console.log(err)
+  })
+})
+
 app.listen(3001,() => {
   console.log("reminder server running")
 });
